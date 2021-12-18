8c0dcab306a5b2491272e2a763f53fad2091e747
박은정 <beanlove97@gmail.com>
2021-12-19 06:33:18 +0900

    Fix: 프로젝트명 변경

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
